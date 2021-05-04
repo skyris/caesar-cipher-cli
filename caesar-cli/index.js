b43330884779faa1ca6b52f900b766e7afd21dd9
Victor Klimov <4klimov@gmail.com>
2021-05-04 19:09:30 +0300

    feat: add pipline function and bind input and output

--- caesar-cli/index.js
+++ caesar-cli/index.js
@@ -1,2 +1,17 @@
-const {argv} = require('./argvHandler')
-console.log(argv);
+const {pipeline} = require('stream');
+const argv = require('./argvParser');
+const readFromFileOrStdin = require('./readFromFileOrStdin');
+const writeToFileOrStdout = require('./writeToFileOrStdout');
+
+pipeline(
+  readFromFileOrStdin(argv.input),
+  writeToFileOrStdout(argv.output),
+  (err, value) => {
+    if (err) {
+      console.error(err.message)
+      process.exit(1);
+    } else {
+      console.log('value: ', value);
+    }
+  }
+)
