7aa7575d7b70ec8a578f54f5a682dc29f7d9b242
Victor Klimov <4klimov@gmail.com>
2021-05-04 19:07:31 +0300

    feat: add writeToFileOrStdout module/function

---
+++ caesar-cli/writeToFileOrStdout.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+function writeToFileOrStdout(outputFile) {
+  if (outputFile) {
+  if (fs.existsSync(outputFile)) {
+    return fs.createWriteStream(outputFile, {flags: 'a'})
+  } else {
+      console.error(`Output file ${outputFile} does not exist!`)
+      process.exit(1)
+    }
+  } 
+  return process.stdout
+
+}
+
+module.exports = writeToFileOrStdout;
